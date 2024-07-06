13aeb3d9018fb5f5183ec44f318c92a0598d51fc
turpeluren <thenotteam@gmail.com>
2024-07-06 23:56:58 +0200

    started editing footer

--- src/navigation.js
+++ src/navigation.js
@@ -111,7 +111,7 @@ export const headerData = {
 
 export const footerData = {
   links: [
-    {
+    /*{
       title: 'Product',
       links: [
         { text: 'Features', href: '#' },
@@ -142,7 +142,7 @@ export const footerData = {
         { text: 'Skills', href: '#' },
         { text: 'Status', href: '#' },
       ],
-    },
+    },*/
     {
       title: 'Company',
       links: [
@@ -164,11 +164,13 @@ export const footerData = {
     { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
     { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
     { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
-    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
-    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
+    /*{ ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
+    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },*/
   ],
   footNote: `
-    <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
-    Made by <a class="text-blue-600 underline dark:text-muted" href="https://onwidget.com/"> onWidget</a> · All rights reserved.
+    Ung i världen · 
+    2024 · 
+    Sida av <a class="text-blue-600 underline dark:text-muted" href="https://turpelurpeluren.online">turpelurpeluren</a>
+    baserat på <a class="text-blue-600 underline dark:text-muted" href="https://github.com/onwidget/astrowind">AstroWind</a>.
   `,
 };
