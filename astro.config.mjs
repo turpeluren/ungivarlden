2bd5154d55d8dfe5706a9dc846dd5f223fc60d10
turpeluren <thenotteam@gmail.com>
2024-08-06 18:28:42 +0200

    static mode ist för server

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ const whenExternalScripts = (items = []) =>
   hasExternalScripts ? (Array.isArray(items) ? items.map((item) => item()) : [items()]) : [];
 
 export default defineConfig({
-  output: 'server',
+  output: 'static',
 
   integrations: [
     tailwind({
